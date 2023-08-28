e0b434b6dfc5fdebf6cc9dea83fbd1a35470338b
Alexandre <134382188+atxmagnus@users.noreply.github.com>
2023-08-28 03:27:07 -0300

    Write code for function to repeat and return a given string for a number  of times.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function (string, times) {
+    let repeatedString = '';
+    if (times < 0) {
+        return 'ERROR'
+    }
+    else {
+        for (let i = 1; i <= times; i++) {
+            repeatedString += string;
+        }
+        return repeatedString;
+    }
 };
 
 // Do not edit below this line
