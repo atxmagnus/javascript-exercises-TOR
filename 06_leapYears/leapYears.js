89427bc8c5b1cd1d339038dc1ed96c33b419e18b
Alexandre <134382188+atxmagnus@users.noreply.github.com>
2023-09-03 06:34:52 -0300

    Write fuction to calculate leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+"use strict";
+const leapYears = function (year) {
+    //must be divisable by four
+    //can not be divisable by 100
+    //but can be divisable by 400
+    if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) {
+        return true;
+    }
+    else
+        return false;
 };
 
 // Do not edit below this line
