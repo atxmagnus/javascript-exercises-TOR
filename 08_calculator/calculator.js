e6731ecef68777e2b3d5f3cffbb4f580ec92fc57
Alexandre <134382188+atxmagnus@users.noreply.github.com>
2023-09-03 08:05:46 -0300

    Write functions of calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+"use strict";
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  let sum = 0;
+  for (let num of array) {
+    sum += num;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  let result = 1;
+  for (let num of array) {
+    result *= num;
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function (num, pow) {
+  return num ** pow;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let result = 1;
+  while (num > 0) {
+    result *= num--;
+  }
+  return result;
 };
 
 // Do not edit below this line
