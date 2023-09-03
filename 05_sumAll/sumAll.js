da854055c87a35cb2a55c21eaecd515e3ce11d7f
Alexandre <134382188+atxmagnus@users.noreply.github.com>
2023-09-03 06:38:48 -0300

    write fuction to sum all numbers passed as parameters

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+"use strict";
+const sumAll = function (intBegin, intEnd) {
+    if (typeof (intBegin) != 'number' || typeof (intEnd) != 'number') {
+        return "ERROR";
+    }
+    else if (intBegin < 0 || intEnd < 0) {
+        return "ERROR";
+    }
+    else {
+        //to iterate from the smaller number
+        if (intBegin > intEnd) {
+            let intBackup = intBegin;
+            intBegin = intEnd;
+            intEnd = intBackup;
+            console.log(`${intBegin}, ${intEnd}`);
+        }
+        let totalSum = 0;
+        while (intBegin <= intEnd) {
+            totalSum += intBegin++;
 
+        }
+        return totalSum;
+    }
 };
 
 // Do not edit below this line
