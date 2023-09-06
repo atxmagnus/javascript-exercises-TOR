ee33f032d2a5b19511fe2f5b666205a2cdb1b7a3
Alexandre <134382188+atxmagnus@users.noreply.github.com>
2023-09-06 07:27:16 -0300

    return fibonacci sequence of given number

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
+    if (number < 0) return "OOPS";
+    let previous1 = 1;
+    let previous2 = 0;
+    let actual;
+    for (let i = 2; i <= number; i++) {
+        actual = previous1 + previous2;
+        previous2 = previous1;
+        previous1 = actual;
+    }
+    return previous1 + previous2;
 
 };
 
