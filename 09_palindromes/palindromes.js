d3c6418b7f86e7eb5c8f8060250b8932ad942a4b
Alexandre <134382188+atxmagnus@users.noreply.github.com>
2023-09-06 06:39:29 -0300

    wrote function to solve test

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let lettersString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+    return lettersString.split("").reverse().join("") == lettersString;
 };
 
 // Do not edit below this line
