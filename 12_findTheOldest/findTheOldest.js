88ba2226845a88662d3657d024b05aff0d9add2d
Alexandre <134382188+atxmagnus@users.noreply.github.com>
2023-09-11 06:42:46 -0300

    Calculate the age of each person in the array object and return the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function (person) {
+    let oldest = 0;
+    for (let i = 0; i < person.length; i++) {
+        const age = person[i].yearOfDeath === undefined ?
+            new Date().getFullYear() - person[i].yearOfBirth :
+            person[i].yearOfDeath - person[i].yearOfBirth;
+        const ageOldest = person[oldest].yearOfDeath === undefined ?
+            new Date().getFullYear() - person[oldest].yearOfBirth :
+            person[oldest].yearOfDeath - person[oldest].yearOfBirth;
 
+        if (age > ageOldest)
+            oldest = i;
+
+    }
+    return person[oldest];
 };
 
 // Do not edit below this line
